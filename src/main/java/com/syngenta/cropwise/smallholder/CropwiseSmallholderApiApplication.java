0fb134d2ddb429036f38abf75446e6fe18766c0b
Bernardo Oliveira <bernardo.oliveira@syngenta.com>
2021-08-06 17:36:45 -0300

    Create base project

---
+++ src/main/java/com/syngenta/cropwise/smallholder/CropwiseSmallholderApiApplication.java
@@ -0,0 +1,13 @@
+package com.syngenta.cropwise.smallholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropwiseSmallholderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropwiseSmallholderApiApplication.class, args);
+	}
+
+}
